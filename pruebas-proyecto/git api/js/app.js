d625f9fed2b03394ff6244b5831aa1dbababfa91
Rocio <rocio.cejudo.soronellas@gmail.com>
2018-09-20 09:15:11 +0200

    javascritp

---
+++ pruebas-proyecto/git api/js/app.js
@@ -0,0 +1,11 @@
+const inputValue = document.querySelector("#seach");
+const searchButton = document.querySelector(".searchButton");
+const nameContainer = document.querySelector(".main__profile-name");
+const unContainer =document.querySelector(".main__profile-username");
+const reposContainer =document.querySelector(".main__profile-repos");
+const urlContainer =document.querySelector(".main__profile-url");
+
+//tdd para el funcionamiento del botón
+// searchButton.addEventListener("click",()=>{
+//     console.log("El botón funciona!");
+// })
